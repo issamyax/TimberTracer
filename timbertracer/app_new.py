a1c2ac54e25d7c56d86a3d85e6ab8f5a5b185789
issamyax <56364364+issamyax@users.noreply.github.com>
2024-09-13 11:37:43 +0100

    Update app_new.py

--- timbertracer/app_new.py
+++ timbertracer/app_new.py
@@ -1,1 +1,16 @@
+#external imports
+import pandas as pd
+
+
+#internal imports
+from ttlibs.dendrometer import dendrometer
+from ttlibs.thinning import perform_thinning
+from ttlibs.bucking import bucking_allocation
+from ttlibs.processor import processor
+from ttlibs.recycling_methane import total_stock_calculator
+from ttlibs.recycling_methane import total_emission_calculator
+from ttlibs.recycling_methane import total_recycling_calculator
+from ttlibs.substitution_dynamic import energy_sub
+from ttlibs.substitution_dynamic import material_sub
+import config as ttdata
 
